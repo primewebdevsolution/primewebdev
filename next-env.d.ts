6f16703352b550bfc486461984667ed8360418b0
Samir khan <infoprimewebdev@gmail.com>
2025-04-23 01:28:06 -0700

    Calendy fixed

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-interface Window {
-    Calendly: any; // or you can define a more specific type if available
+declare global {
+  interface Window {
+    Calendly: any;
   }
-  
+}
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
